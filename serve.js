3a77e7dd7b13f7ac8cb36b4642a6dbb668f4d1f8
unknown <saidocho@gmail.com>
2020-02-06 16:07:53 -0600

    funciona pls

--- serve.js
+++ serve.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', serveStatic(path.join(_dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 000
+const port = process.env.PORT || 8080
 app.listen(port)
 
 console.log('Listening on port:' + port)
