2276ced61b485c80d5f42175000d64a3adac9480
unknown <saidocho@gmail.com>
2020-02-06 13:35:06 -0600

    prueba imagen header

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+  
+     <div id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-header>
+
+
+
+      </a-layout-header>
+      
+      <a-layout-content>  
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:rgba(255,255,255,0);">
+  <a class="navbar-brand" href="#"><img src="../src/assets/logoo blanco.png" id="logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"> ANOVA PUNTO DE VENTA<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ANOVA EMPRESARIAL</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#">HARDWARE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PRECIOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">SOPORTE</a>
+      </li>
+        <li class="nav-item">
+       <a class="nav-link"> 
+   <a-popover placement="bottom">
+        <template slot="content">
+           <center>
+          <span><a-icon type="phone" />(33) 20 14 89 95 </span> </center>
+        </template>
+     
+        <a-button><a-icon type="customer-service" />¡LLámanos!</a-button>
+      </a-popover>
+       </a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+      
+<!-- 
+         <a-card title="" style="width:100%;background-color:rgba(255,255,255,0);">
+    <a-card-grid style="width:50%;">
+      <p id="p1"><a-icon type="ellipsis" />Controlar tu restaurante<br>nunca fue tan facil</p>
+     
+
+      </a-card-grid>
+    <a-card-grid style="width:50%;"><img
+      alt="example"
+      src="compu.png"
+      
+    /></a-card-grid>
+
+          </a-card>
+         -->
+    <a-row>
+      <a-col :span="12"><p id="p1"><a-icon type="ellipsis" />Controlar tu restaurante nunca fue tan facil</p>
+</a-col>
+      <a-col :span="12"><img
+      alt="example"
+      src="compu.png"
+      
+    /></a-col>
+    </a-row>
+          
+          </a-layout-content>
+
+          
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+     </div>
+
+  </div>
+</template>
+
+<script>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+     
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  
+
+  #menu{
+      background-image: linear-gradient(to bottom right, red, yellow);
+;
+  }
+ 
+  #components-layout-demo-basic {
+    text-align: center;
+  }
+  #components-layout-demo-basic .ant-layout-header{
+  background-image: linear-gradient(to bottom right, red, yellow);
+
+        color: #fff;
+  }
+  #components-layout-demo-basic .ant-layout-footer {
+    color: #fff;
+  }
+  #components-layout-demo-basic .ant-layout-footer {
+    line-height: 1.5;
+  }
+  #components-layout-demo-basic .ant-layout-sider {
+    color: #fff;
+    line-height: 120px;
+  }
+  #components-layout-demo-basic .ant-layout-content {
+    color: #fff;
+    min-height: 120px;
+    line-height: 120px;
+  }
+  #components-layout-demo-basic > .ant-layout {
+    margin-bottom: 48px;
+  }
+  #components-layout-demo-basic > .ant-layout:last-child {
+    margin: 0;
+  }
+</style>
